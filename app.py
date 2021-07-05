8efeb4624222148fc40e616f3beafe1e1c958401
malcarb <75375889+malcarb@users.noreply.github.com>
2021-07-05 12:55:32 -0300

    session initialization in vcenter_connection()

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ def dataStoresPrint():
 
 
 def vcenter_connection(host):
+    session = requests.session()
     vsphere_client = create_vsphere_client(server=host, username=os.environ['api_user'], password=os.environ['api_pwd'], session=session)
     lhost = vsphere_client.vcenter.Host.list()
     final_keys=[]
